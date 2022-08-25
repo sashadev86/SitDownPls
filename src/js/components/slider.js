2187bd12219e13c9ebc5cc4b42808ef5836a5840
Aliaksandr <djvalentinoff@gmail.com>
2022-08-25 11:04:04 +0300

    feat: add cutomPagination slider hero

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,53 +1,38 @@
 import Swiper, { Pagination, Autoplay, Keyboard, A11y } from 'swiper';
 Swiper.use([Pagination, Autoplay, Keyboard, A11y]);
-// const swiper = new Swiper('.hero__swiper', {
-//   slidesPerView: 1,
-//   pagination: {
-//     el: '.swiper-pagination',
-//     type: 'bullets',
-//     clickable: true,
-//   },
-//   // allowTouchMove: false,
-//   loop: true,
-//   // autoplay: {
-//   //   delay: 5000
-//   // },
-//   keyboard: {
-//     enabled: true,
-//     onlyInViewport: false,
-//   },
-//   a11y: {
-//     prevSlideMessage: 'Previous slide',
-//     nextSlideMessage: 'Next slide',
-//   },
-// });
 
-
-  let swiper01 = document.querySelector('.hero__swiper');
-  if (swiper01) {
-    swiper01 = new Swiper('.hero__swiper', {
-      watchOverflow: true,
-      loop: true,
-      autoplay: true,
-      speed: 800,
-      effect: 'fade',
-      slidesPerView: 1,
-      spaceBetween: 10,
-      lazy: false,
-      pagination: {
-        el: '.hero__pagination',
-        type: 'bullets',
-        renderBullet(index, className) {
-          return `
-            <span class=${className}>
-            <svg class="circular-pagination__svg" width="24" height="24" viewBox="0 0 24 24">
-              <circle class="circular-pagination__bg" r="10" cx="12" cy="12" fill="none" stroke-width="2"></circle>
-              <circle class="circular-pagination__progress" r="10" cx="12" cy="12" fill="none" stroke-width="3"></circle>
-            </svg>
-            </span>
-          `;
-        },
-      },
-    });
-  }
+let swiperHero = document.querySelector('.hero__swiper');
+if (swiperHero) {
+  swiperHero = new Swiper('.hero__swiper', {
+    watchOverflow: true,
+    slidesPerView: 1,
+    loop: true,
+    speed: 1000,
+    autoplay: {
+      delay: 2000
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    a11y: {
+      prevSlideMessage: 'Предыдущий слайд',
+      nextSlideMessage: 'Следующий слайд',
+      firstSlideMessage: 'Это первый слайд',
+      lastSlideMessage: 'Это последний слайд',
+      paginationBulletMessage: 'Перейти к слайду {{index}}',
+    },
+    pagination: {
+      el: '.hero__pagination',
+      type: 'bullets',
+      clickable: true,
+      renderBullet(index, className) {
+        return `<span class="${className}"><svg width="24" height="24" class="circ">
+        <circle class="circ-bg" r="10" cx="12" cy="12" fill="none" stroke-width="2.3"/>
+        <circle class="circ1" r="10" cx="12" cy="12" stroke="var(--secondary-color)" stroke-width="2.3" fill="none"/>
+        </svg></span>`;
+      }
+    },
+  });
+}
 
