0db4e2d6a8d2175dee446b977cc30bdcf2166758
Aliaksandr <djvalentinoff@gmail.com>
2022-08-29 17:33:50 +0300

    feat: add section raiting decstop

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,2 +1,3 @@
 import './components/select';
 import './components/slider';
+import './components/cardMore';
---
+++ src/js/components/cardMore.js
@@ -0,0 +1,24 @@
+function isHidden(el) {
+  const style = window.getComputedStyle(el);
+  return (style.display === 'none');
+}
+
+function setShowMore(showQuantity) {
+  const btn = document.querySelector('.js-show-btn');
+
+  btn.addEventListener("click", function () {
+      const boxes = Array.from(document.querySelectorAll('.card__item'));
+      const hiddenIndex = boxes.findIndex(box => isHidden(box));
+
+      for (let k = hiddenIndex; k < hiddenIndex + showQuantity; k++) {
+          if (boxes[k]) {
+              boxes[k].style.display = 'block';
+          } else {
+              btn.style.display = 'none';
+              break;
+          }
+      }
+  })
+}
+
+setShowMore(4);
