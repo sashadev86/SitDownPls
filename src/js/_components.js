02f62e34cdc60fa3441628fd37eca10bee0174c1
Aliaksandr <djvalentinoff@gmail.com>
2022-09-05 16:07:59 +0300

    feat: add section feedback and add form section feedback

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,4 @@
 import './components/select';
 import './components/slider';
 import './components/cardMore';
+import './components/validation';
---
+++ src/js/components/validation.js
@@ -0,0 +1,51 @@
+import { validateForms } from '../functions/validate-forms';
+const rules = [
+  {
+    ruleSelector: '.input-name',
+    rules: [
+      {
+        rule: 'minLength',
+        value: 3,
+        errorMessage: 'Минимальное количество символов 3'
+      },
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните имя!'
+      }
+    ]
+  },
+  {
+    ruleSelector: '.input-tel',
+    tel: true,
+    telError: 'Введите корректный телефон',
+    rules: [
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните телефон!'
+      }
+    ]
+  },
+  {
+    ruleSelector: '.input-email',
+    rules: [
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните email!'
+      },
+      {
+        rule: 'email',
+        value: true,
+        errorMessage: 'Введите корректный email!'
+      }
+    ]
+  },
+];
+
+const afterForm = () => {
+  console.log('Произошла отправка, тут можно писать любые действия');
+};
+
+validateForms('.feedback__form', rules, afterForm);
