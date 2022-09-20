9081658f1b6dd035b54c4654692ec4b898e3220b
Aliaksandr <djvalentinoff@gmail.com>
2022-09-20 17:48:28 +0300

    fix: refactor header desctop first and two lines

--- src/js/components/select.js
+++ src/js/components/select.js
@@ -1,21 +1,23 @@
 //Селект Москва
-const citySelect = () => {
-  const element = document.querySelector('.header__city-select');
-  const choices = new Choices(element, {
-    searchEnabled: false,//убирает Ентер*/
-    /*работает если отключить searchEnabled: false. будет поиск по селекту*/
-    noResultsText: 'Ничего не найдено'
-  });
-};
+const choosingRegionFirst = document.querySelector('.choosing__region-wrap--first');
 
-citySelect();
+if (choosingRegionFirst) {
+  const select = choosingRegionFirst.querySelector('.choosing__region-select');
+
+  const choices = new Choices(select, {
+    allowHTML: false,
+    searchEnabled: false,
+    shouldSort: false,
+    itemSelectText: '',
+  });
+}
 
 //Селект Диваны
-const selector = document.querySelector(".header__сategories-select")
+// const selector = document.querySelector(".header__сategories-select")
 
-const choices = new Choices(selector, {
-  searchEnabled: false,
-  classNames: {
-    containerOuter: 'choices header__сategories-form',
-  },
-});
+// const choices = new Choices(selector, {
+//   searchEnabled: false,
+//   classNames: {
+//     containerOuter: 'choices header__сategories-form',
+//   },
+// });
