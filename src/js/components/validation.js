ef9da55fd36e82c1659030b1bdd501db4e3f8826
Aliaksandr <djvalentinoff@gmail.com>
2022-10-05 14:35:48 +0300

    feat: add validation form in product card

--- src/js/components/validation.js
+++ src/js/components/validation.js
@@ -44,6 +44,35 @@ const rules = [
     ]
   },
 ];
+const rules1 = [
+  {
+    ruleSelector: '.input-name',
+    rules: [
+      {
+        rule: 'minLength',
+        value: 3,
+        errorMessage: 'Минимальное количество символов 3'
+      },
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните имя!'
+      }
+    ]
+  },
+  {
+    ruleSelector: '.input-tel',
+    tel: true,
+    telError: 'Введите корректный телефон',
+    rules: [
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните телефон!'
+      }
+    ]
+  },
+];
 
 const afterForm = () => {
   console.log('Произошла отправка, тут можно писать любые действия');
@@ -52,4 +81,9 @@ const afterForm = () => {
 let feedbackForm = document.querySelector('.feedback__form');
 if(feedbackForm) {
   validateForms('.feedback__form', rules, afterForm);
+}
+
+let buyForm = document.querySelector('.buy-form');
+if(buyForm) {
+  validateForms('.buy-form', rules1, afterForm);
 }
