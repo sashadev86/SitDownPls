e7b04c97c4d62a3fc56096482d8f2374c2b9a143
Aliaksandr <djvalentinoff@gmail.com>
2022-10-10 15:46:31 +0300

    feat: add adaptive proader and fix button more in section rating

--- src/js/components/cardMore.js
+++ src/js/components/cardMore.js
@@ -1,26 +1,53 @@
-function isHidden(el) {
-  const style = window.getComputedStyle(el);
-  return (style.display === 'none');
+const highRatingSection = document.querySelector('.rating');
+
+const firstBreakpoint = '(min-width: 100px)';
+const secondBreakpoint = '(min-width: 993px)';
+const thirdBreakpoint = '(min-width: 1201px)';
+
+function showingCardsByClick(showOnLoad = 8, showMore = 4) {
+  const SHOW_ON_LOAD = showOnLoad;
+  const SHOW_MORE = showMore;
+
+  const cardItems = [...highRatingSection.querySelectorAll('.rating-card')];
+  const showMoreButton = highRatingSection.querySelector('.js-show-btn');
+
+  function showItems(count) {
+    cardItems.forEach((el) => {
+      el.classList.remove('product-card-active');
+    });
+
+    cardItems.splice(0, count).forEach((el) => {
+      el.classList.add('product-card-active');
+    });
+
+    showMoreButton.classList.remove('show-more-hidden');
+
+    if (!cardItems.length) {
+      showMoreButton.classList.add('show-more-hidden');
+    }
+  }
+
+  showItems(SHOW_ON_LOAD);
+
+  showMoreButton.addEventListener('click', () => {
+    showItems(SHOW_MORE);
+  });
 }
 
-function setShowMore(showQuantity) {
-  const btn = document.querySelector('.js-show-btn');
-  if(btn) {
-
-    btn.addEventListener("click", function () {
-      const boxes = Array.from(document.querySelectorAll('.card__item'));
-      const hiddenIndex = boxes.findIndex(box => isHidden(box));
-
-      for (let k = hiddenIndex; k < hiddenIndex + showQuantity; k++) {
-        if (boxes[k]) {
-          boxes[k].style.display = 'block';
-        } else {
-          btn.style.display = 'none';
-          break;
-        }
-      }
-    })
+function changingValuesOnBreakpoints() {
+  if (window.matchMedia(thirdBreakpoint).matches) {
+    showingCardsByClick();
+  } else if (window.matchMedia(secondBreakpoint).matches) {
+    showingCardsByClick(6, 3);
+  } else if (window.matchMedia(firstBreakpoint).matches) {
+    showingCardsByClick(6, 2);
   }
 }
 
-setShowMore(4);
+window.addEventListener('DOMContentLoaded', () => {
+  changingValuesOnBreakpoints();
+});
+
+window.addEventListener('resize', () => {
+  changingValuesOnBreakpoints();
+});
